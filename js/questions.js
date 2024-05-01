31c3b89253004dc1a90a05d62cad2c2643ccf9a6
Paramhans Singh <paramhanssingh084@gmail.com>
2024-05-01 20:56:52 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,96 @@
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Which of the following sports facilities is not available at MAIT?",
+    answer: "D) Swimming Pool",
+    options: [
+      "A) Basketball Court",
+      "B) Tennis Court",
+      "C) Cricket Ground",
+      "D) Swimming Pool",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Which university is MAIT affiliated with?",
+    answer: "A) Guru Gobind Singh Indraprastha University (GGSIPU)",
+    options: [
+      "A) Guru Gobind Singh Indraprastha University (GGSIPU)",
+      "B) Delhi Technological University (DTU)",
+      "C) Jamia Millia Islamia (JMI)",
+      "D) Jawaharlal Nehru Technological University (JNTU)",
+    ],
+  },
+  {
+    numb: 3,
+    question: "Which one of them is a cultural society of MAIT?",
+    answer: "C) Literary Umbrella",
+    options: ["A) IOSD", "B) GDSC", "C) Literary Umbrella", "D) IEEE"],
+  },
+  {
+    numb: 4,
+    question:
+      "What year was Maharaja Agrasen Institute of Technology (MAIT) established?",
+    answer: "B) 1999",
+    options: ["A) 1997", "B) 1999", "C) 2001", "D) 2003"],
+  },
+  {
+    numb: 5,
+    question: "Which campus is MAIT located in?",
+    answer: "B) Rohini",
+    options: ["A) Dwarka", "B) Rohini", "C) Janakpuri", "D) Saket"],
+  },
+  {
+    numb: 6,
+    question: "What does NSS stand for at MAIT?",
+    answer: "A) National Service Scheme",
+    options: [
+      "A) National Service Scheme",
+      "B) National Science Society",
+      "C) National Student Society",
+      "D) National Sports Society",
+    ],
+  },
+  {
+    numb: 7,
+    question:
+      "Who is the founder of Maharaja Agrasen Institute of Technology (MAIT)?",
+    answer: "C) Maharaja Agrasen Technical Education Society",
+    options: [
+      "A) Maharaja Agrasen",
+      "B) Maharaja Surajmal",
+      "C) Maharaja Agrasen Technical Education Society",
+      "D) Maharaja Agrasen Hospital",
+    ],
+  },
+  {
+    numb: 8,
+    question: "Which of the following is not a course offered at MAIT?",
+    answer: "D) Bioinformatics Engineering",
+    options: [
+      "A) Computer Science and Engineering",
+      "B) Electronics and Communication Engineering",
+      "C) Mechanical Engineering",
+      "D) Bioinformatics Engineering",
+    ],
+  },
+  {
+    numb: 9,
+    question:
+      "Which of the following is one of the technical societies at MAIT?",
+    answer: "A) GDSC",
+    options: ["A) GDSC", "B) Ayaam", "C) Affinity", "D) Funk in Motion"],
+  },
+  {
+    numb: 10,
+    question: "What is the name of the annual technical fest at MAIT?",
+    answer: "D) TnM-TechSurge n Mridang",
+    options: [
+      "A) Technotsav",
+      "B) Innovision",
+      "C) Technovision",
+      "D) TnM-TechSurge n Mridang",
+    ],
+  },
+];
